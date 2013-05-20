8292291cc21ef374c3f72d4816a283b0f2eb0a90
EddPorter <github@eddporter.com>
2013-05-20 07:29:50 +0100

    Add `Horse` information class

---
+++ RacingData/Horse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RacingData {
+
+  public class Horse {
+
+    public int Age {
+      get;
+      set;
+    }
+
+    public string Breed {
+      get;
+      set;
+    }
+
+    public string CountryOfBirth {
+      get;
+      set;
+    }
+
+    public DateTime DateOfBirth {
+      get;
+      set;
+    }
+
+    public virtual Horse Father {
+      get;
+      set;
+    }
+
+    public virtual Horse Mother {
+      get;
+      set;
+    }
+
+    public string Name {
+      get;
+      set;
+    }
+
+    // form
+    // races
+  }
+}
