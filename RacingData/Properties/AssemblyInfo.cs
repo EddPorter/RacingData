09f79afc1249e8d5a456d01dd78596b97838fa02
EddPorter <github@eddporter.com>
2013-05-20 07:21:49 +0100

    Create initial solution structure

---
+++ RacingData/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Created 20 May 2013
+[assembly: AssemblyVersion("0.0.1.1")]
+[assembly: AssemblyFileVersion("0.0.1.1")]
+[assembly: AssemblyTitle("RacingData")]
+[assembly: AssemblyDescription("Library for interfacing with horse racing sites and presenting the data in a programaticaly usable form.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Edd Porter")]
+[assembly: AssemblyProduct("Racing Suite")]
+[assembly: AssemblyCopyright("Copyright © Edd Porter 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("42c07d7d-d461-4dbd-922a-7180924234da")]
+[assembly: CLSCompliant(true)]
